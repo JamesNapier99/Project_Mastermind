f10c3068bb42ef29d6e83398ab1811d3d6f38f59
napier.j <napier.j@northeastern.edu>
2022-09-15 12:23:07 -0400

    initial creation of mastermind_a.cppp. Includes rudimentary start to class architecture

---
+++ mastermind_a.cpp
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////
+// Project Mastermind: Part A
+// Written by [placeholder],[placeholder],[placeholder]
+///////////////////////////////////////////////////////
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Code
+{
+private:
+    int checkIncorrect(Code code);
+    int checkCorrect(Code code);
+    void initializeCode(int n, int m);
+    vector<int> CODE;
+public:
+    Code (int n, int m);
+};
+
+ Code::Code (int n, int m)
+{
+}
+
+
+int main() {
+    //call constructor
+    //initialize CODE for code
+}
