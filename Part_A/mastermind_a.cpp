819dd12c980a21f34b5a43d56d0eb5ec8b07f913
Sam Sheehan <SamSheehan@macbook-pro-3.mynetworksettings.com>
2022-09-23 15:18:39 -0400

    Removed promptGuess, changed main

--- Part_A/mastermind_a.cpp
+++ Part_A/mastermind_a.cpp
@@ -6,9 +6,9 @@
 // Julia Rasmussen:                                rasmussen.j@northeastern.edu
 // Samuel Sheehan:                                   sheehan.s@northeastern.edu
 //
-// Main program file for projet 1. Contains declaration for the 
-// code constructor, initializeCode, checkCorrect, checkIncorrect, promptGuess,
-// and print
+// Main program file for project 1. Contains declaration for the
+// code constructor, initializeCode, checkCorrect, checkIncorrect,
+// print and writeVec.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -20,7 +20,7 @@
 using namespace std;
 using namespace n;
 
-code::code (int n, int m)
+code::code (const int &n, const int &m)
 // constructor for code class
 {
     LENGTH = n;
@@ -37,30 +37,37 @@ void code::initializeCode()
     for (int i = 0; i < LENGTH; i++)
     {
         CODE[i] = rand() % RANGE;
-    }
+    } //end for
 }
 
-int code::checkCorrect (code &guess)
-// Compares index i of the given code object's CODE field with 
-// index i of this code object's CODE field for 0<=i<LENGTH
+int code::checkCorrect (const code &guess)
+// Compares index i of the inputted code guess object's CODE field with
+// index i of this code object's CODE field for 0<=i<LENGTH and returns
+// the number of equivalent values.
 {
     int numCorrect = 0;
+
     for (int i = 0; i < LENGTH; i++)
     {
         if (guess.CODE[i] == CODE[i])
         {
             numCorrect++;
-        }
-    }
+        } //end if
+    } //end for
     return numCorrect;
 }
 
-//returns the # of digits in the incorrect position
-int code::checkIncorrect(code& guess)
+int code::checkIncorrect(code &guess)
+// First, removes values that are correctly placed in inputted code guess'
+// CODE field. Then, compares the remaining values in the inputted code to
+// the master code, and returns how many common values there are,
+// i.e incorrectly placed digits in the guess.
 {
     int numIncorrect = 0;
     int count = LENGTH;
     vector<int> temp = CODE;
+
+    //removes common values from each CODE field
     for (int i = 0; i < count; i++)
     {
         if (guess.CODE[i] == temp[i]) {
@@ -68,9 +75,13 @@ int code::checkIncorrect(code& guess)
             temp.erase(temp.begin() + i);
             i--;
             count--;
-        }
-    }
+        } //end if
+    } //end for
 
+    // Loops through both the guess CODE field and the master CODE field,
+    // comparing values to count how many are equivalent with different
+    // indices. When a common value is found, it is set to a value outside
+    // of the given range of values as to avoid double counting.
     for (int i = 0; i < count; i++)
     {
         for (int j = 0; j < count; j++)
@@ -80,82 +91,56 @@ int code::checkIncorrect(code& guess)
                 temp[j] = RANGE + 2;
                 numIncorrect++;
                 break;
-            }
-        }
-    }
+            } //end if
+        } //end for
+    } //end for
     return numIncorrect;
 }
 
-//prompts user to enter a guess of the code.
-void code::promptGuess()
-{
-    //loops through code at most 10 times.
-    while (COUNT < 10)
-    {
-        COUNT += 1;
-        code guess(LENGTH, RANGE);
-
-        //print code
-        cout << "\nOur super secret code is ";
-        print();
-
-        //fills in guess with a total of LENGTH integers.
-        for (int i = 0; i < LENGTH; i++)
-        {
-            int temp;
-            //prompt user for value at index i of guess.
-            cout << "\nPlease enter index "<< i + 1 <<" of guess #" << COUNT << ": ";
-            cin >> temp;
-
-            //push int to the back of guess vector.
-            guess.CODE[i] = temp;
-        }
-
-        int numCorrect = checkCorrect(guess);
-        int numIncorrect = checkIncorrect(guess);
-
-        cout << "\nNumber of correct digits: " << numCorrect;
-        cout << "\nNumber of digits in wrong place: " << numIncorrect;
-
-        //checks whether or not the given guess is correct. if so, win condition.
-        if (numCorrect == LENGTH)
-        {
-            COUNT = 10;
-            cout << "\nYou guessed the secret number! Congrats!";
-            cout << "\n================================================================================";
-            return;
-        }
-        else
-        {
-            cout << "\nYour guess was not correct. You have " << 10 - COUNT << " guesses left!";
-            cout << "\n================================================================================";
-        }
-    }
-    //the lose condition.
-    cout << "\nSorry, you ran out of guesses. Try again next time!";
-    cout << "\n================================================================================";
-}
-
-//prints the code.
 void code::print()
+// Prints the values in the CODE vector field.
 {
     for (int i = 0; i < LENGTH; i++)
     {
         cout << CODE[i];
-    }
+    } //end for
 }
 
-//the main function of the program.
-int main()
+void code::writeVec(const vector<int> &guess)
+// Writes a vector to the CODE vector field of a code object.
 {
-    int len, ran;
-    cout << "================================================================================";
-    cout << "\nPlease enter the length of the guess you want to play with: ";
-    cin >> len;
-    cout << "Please enter the range of numbers you want to play with: ";
-    cin >> ran;
-    cout << "================================================================================";
+    for (int i = 0; i < LENGTH; i++)
+    {
+        CODE[i] = guess[i];
+    } //end for
+}
+
+int main()
+//the main function of the program.
+{
+    int len = 5;
+    int ran = 7;
     code master(len, ran);
     master.initializeCode();
-    master.promptGuess();
+    code guess1(len, ran);
+    code guess2(len, ran);
+    code guess3(len, ran);
+    vector<int> g1{5, 0, 3, 2, 6};
+    vector<int> g2{2, 1, 2, 2, 2};
+    vector<int> g3{1, 3, 3, 4, 5};
+    guess1.writeVec(g1);
+    guess2.writeVec(g2);
+    guess3.writeVec(g3);
+    cout << "For sample guess 1, there are " << master.checkCorrect(guess1)
+        << " correctly placed digits and " << master.checkIncorrect(guess1)
+        << " incorrectly placed digits.\n";
+    cout << "For sample guess 2, there are " << master.checkCorrect(guess2)
+         << " correctly placed digits and " << master.checkIncorrect(guess2)
+         << " incorrectly placed digits.\n";
+    cout << "For sample guess 3, there are " << master.checkCorrect(guess3)
+         << " correctly placed digits and " << master.checkIncorrect(guess3)
+         << " incorrectly placed digits.\n";
+    cout << "\nThe correct code was: ";
+    master.print();
+    cout << "\n";
 }
