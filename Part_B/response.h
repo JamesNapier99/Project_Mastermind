b767d20fc7c2e27284ea14eb62dcc07e92bd5cb3
JamesNapier99 <jamesnapier99@gmail.com>
2022-09-26 13:35:30 -0400

    adding namespace to headers, continuing to flesh out classes

--- Part_B/response.h
+++ Part_B/response.h
@@ -3,19 +3,20 @@
 #define RESPONSE_H
 
 #include <vector>
-
-class response
-{
-private:
-    int numCorrect;
-    int numIncorrect;
-public:
-    response(int c, int i);
-    void setCorrect(int correct);
-    void setIncorrect(int incorrect);
-    int getCorrect();
-    int getIncorrect();
-    bool operator == (const response rhs);
-    void operator << ();
-};
+namespace n {
+    class response
+    {
+        private:
+            int numCorrect;
+            int numIncorrect;
+        public:
+            response(int c, int i);
+            void setCorrect(int correct);
+            void setIncorrect(int incorrect);
+            int getCorrect();
+            int getIncorrect();
+            bool operator == (const response rhs);
+            void operator << ();
+    };
+}
 #endif
